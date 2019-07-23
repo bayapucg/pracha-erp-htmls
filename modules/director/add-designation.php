7015b26e248b6f041760b81c857a5983b789be25
Aryasatheesan <aryasatheesan12345@gmail.com>
2019-07-23 20:47:45 +0530

    submit
    update

---
+++ modules/director/add-designation.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include('head.php'); ?>
+</head>
+<body>
+	<?php include('header.php'); ?>
+	<?php include('sidebar.php'); ?>
+			<div class="main-container">
+			<div class="pd-ltr-20 customscroll customscroll-10-p height-100-p xs-pd-20-10">
+						<div class="pd-20 bg-white border-radius-4 box-shadow mb-30">
+						<div class="clearfix">
+						<div class="pull-left">
+							<h4 class="text-blue mb-30">Add Designation</h4>
+						</div>
+						
+					</div>
+					<form>
+						<div class="row">
+							<div class="col-md-6 col-sm-12">
+								<div class="form-group">
+									<label>Add Designation</label>
+									<input type="text" class="form-control" placeholder="Enter Designation">
+								</div>
+							</div>
+							  <div class="form-group col-md-12">
+                            <div class="col-lg-9 col-lg-offset-3">
+                                <button type="submit" class="btn btn-primary" name="signup" value="Sign up">Add</button>
+                              
+                            </div>
+                        </div>
+						</div>
+
+						
+					</form>
+				</div>
+	<?php include('footer.php'); ?>
+	</div>
+	</div>
+	<?php include('script.php'); ?>
+	<script src="src/plugins/highcharts-6.0.7/code/highcharts.js"></script>
+	<script src="src/plugins/highcharts-6.0.7/code/highcharts-more.js"></script>
+	
+</body>
+</html>
---
+++ modules/director/sidebar.php
@@ -0,0 +1,98 @@
+	<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="index.php">
+				<img src="../../vendors/images/deskapp-logo.png" alt="">
+			</a>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Home</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="index.php">Dashboard style 1</a></li>
+							<li><a href="index2.php">Dashboard style 2</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Add Role</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add-department.php">Add Department</a></li>
+							<li><a href="add-designation.php">Add Designation</a></li>
+							<li><a href="add-level.php">Add Level</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Add Employee</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="addrole.php">Add Employee</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Add HR</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="addhr.php">Add HR</a></li>
+							<li><a href="hrlist.php">List</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">RBH Leaves</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="addrole.php">Leave Requests</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Work Management</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="addrole.php">Asign Work</a></li>
+							<li><a href="addrole.php">Work Status</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Recruiting Management</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="addrole.php">Asign Work</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Sales</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="addrole.php">Sales Progress</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Payroll & Payslips</span>
+						</a>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Chat</span>
+						</a>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Feedback</span>
+						</a>
+					</li>
+					
+				</ul>
+			</div>
+		</div>
+	</div>
