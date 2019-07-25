c69610286e084498dc9f35ca7e0d8bad6803fea4
anukulkarni35 <39791000+anukulkarni35@users.noreply.github.com>
2019-07-25 15:42:51 +0530

    update sidebar dashboard
    update sidebar dashboard

--- modules/employee/sidebar.php
+++ modules/employee/sidebar.php
@@ -8,14 +8,10 @@
 			<div class="sidebar-menu">
 				<ul id="accordion-menu">
 					<li class="dropdown">
-						<a href="javascript:;" class="dropdown-toggle">
+						<a href="index.php" class="dropdown-toggle">
 							<span class="fa fa-home"></span><span class="mtext">Dashboard</span>
 						</a>
-						<ul class="submenu">
-							<li><a href="index.php">Dashboard style 1</a></li>
-							<li><a href="index2.php">Dashboard style 2</a></li>
-						</ul>
-					</li>
+						</li>
 					<li class="dropdown">
 						<a href="creatework.php" class="dropdown-toggle">
 							<span class="fa fa-home"></span><span class="mtext">Work Management</span>
