98a0fc15fa4b63d79dc711517fe9631954e7197a
anukulkarni35 <39791000+anukulkarni35@users.noreply.github.com>
2019-07-29 16:15:45 +0530

    update
    changes

--- modules/employee/sidebar.php
+++ modules/employee/sidebar.php
@@ -20,13 +20,10 @@
 					</li>
 					
 					<li class="dropdown">
-						<a href="javascript:" class="dropdown-toggle">
+						<a href="leave.php" class="dropdown-toggle">
 							<span class="fa fa-home"></span><span class="mtext">Leaves & Time-Off</span>
 						</a>
-						<ul class="submenu">
-							<li><a href="leave.php">Leave</a></li>
-							<li><a href="timeoff.php">Time-Off</a></li>
-						</ul>
+						
 					</li>
 					<li class="dropdown">
 						<a href="payroll.php" class="dropdown-toggle">
